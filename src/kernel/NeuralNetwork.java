993f05689f11ef2f7171eec10884e3b74cf480db
MauricioYuji-Code <mauricio.nakandakari2@gmail.com>
2021-12-16 12:09:34 -0300

    comentários neural network

--- src/kernel/NeuralNetwork.java
+++ src/kernel/NeuralNetwork.java
@@ -7,18 +7,41 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * Classe de base para redes neurais artificiais
+ *
+ * @see NeuralNetwork
+ */
 public class NeuralNetwork implements Serializable {
 
+    /**
+     * Coleção de camadas
+     */
     protected ArrayList<Layer> layers;
 
+    /**
+     * Coleção de amostras
+     */
     protected ArrayList<ArrayList<Double>> samples;
 
+    /**
+     * Rótulo
+     */
     protected String label;
 
+    /**
+     * Objeto para a camada de entrada
+     */
     protected Input input;
 
+    /**
+     * Objeto para a camada de saída
+     */
     protected Output output;
 
+    /**
+     * Cria uma instância da rede neural vazia e criando a lista de camadas
+     */
     public NeuralNetwork() {
         this.layers = new ArrayList<>();
     }
@@ -86,10 +109,10 @@ public class NeuralNetwork implements Serializable {
      *
      * @param index indice
      */
-    //Todo verificar quando tiver input
-    //Todo verificar quando tiver output
-    //Todo verificar quando tiver apenas uma layer
     public boolean removeLayerAt(int index) {
+        //Todo verificar quando tiver input
+        //Todo verificar quando tiver output
+        //Todo verificar quando tiver apenas uma layer
         if (layers.get(index).equals(null))
             return false;
         if (index == 0 || index == layers.size() - 1)
@@ -103,6 +126,12 @@ public class NeuralNetwork implements Serializable {
         return true;
     }
 
+    /**
+     * Conetar duas camadas
+     *
+     * @param l1 camada 1
+     * @param l2 camada 2
+     */
     protected void connect(Layer l1, Layer l2) {
         for (int i = 0; i < l1.getNeuronsCount(); i++) {
             for (int j = 0; j < l2.getNeuronsCount(); j++) {
@@ -113,7 +142,7 @@ public class NeuralNetwork implements Serializable {
     }
 
     /**
-     * retorna posicao da camada
+     * Retorna posição da camada
      *
      * @param layer camada
      */
@@ -122,14 +151,17 @@ public class NeuralNetwork implements Serializable {
     }
 
     /**
-     * Retorna numero de camadas
+     * Obter numero de camadas
      */
     public int getLayersCount() {
         return layers.size();
     }
 
     /**
-     * recebe a camada de entrada
+     * Recebe a camada de entrada
+     *
+     * @param input Camada de entrada
+     * @throws Exception já existe camada de entrada
      */
     public void attachInput(Input input) throws Exception {
         if (this.input == null) {
@@ -142,7 +174,10 @@ public class NeuralNetwork implements Serializable {
     }
 
     /**
-     * recebe a camada de saída
+     * Recebe a camada de saída
+     *
+     * @param output Camada de saída
+     * @throws Exception já existe camada de saída
      */
     public void attachOutput(Output output) throws Exception {
         if (this.output == null) {
@@ -153,6 +188,12 @@ public class NeuralNetwork implements Serializable {
         }
     }
 
+    /**
+     * Mostra informações da arquitetura da rede neural
+     *
+     * @param verbose verdadeiro se a informção for mais detalhada e falso se não
+     * @return informações da arquitetura
+     */
     public String showInfo(boolean verbose) {
         StringBuilder info = new StringBuilder();
         info.append("Neural Network Info: \n");
@@ -184,27 +225,44 @@ public class NeuralNetwork implements Serializable {
 
     /**
      * Checar todos os ponteiros de neuronios e conexões
+     *
+     * @return verdeiro se toda a arquiterura estiver integro
      */
-    //Todo verificar integridade das conexões
     public boolean checkIntegrity() {
+        //Todo verificar integridade das conexões
         return true;
     }
 
+    /**
+     * Realização do treinamento
+     */
     public void training() {
         Training training = new Training(this);
-
     }
 
-    /******Getters e Setters*****/
-
+    /**
+     * Obter coleção de camadas
+     *
+     * @return camadas
+     */
     public List<Layer> getLayers() {
         return Collections.unmodifiableList(this.layers);
     }
 
+    /**
+     * Rótulo da rede neural
+     *
+     * @return rótulo
+     */
     public String getLabel() {
         return label;
     }
 
+    /**
+     * Definir rótulo
+     *
+     * @param label rótulo
+     */
     public void setLabel(String label) {
         this.label = label;
     }
